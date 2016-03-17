6c3f4bb4cfef06d549fcef1fbc4c51ba703bf5a5
geobil <gsarcinschi@gmail.com>
2016-03-17 18:23:51 +0200

    DuplicateFinder
    Given an array of N numbers that contains all numbers between 1 to N-1
    write a function that finds the duplicates

---
+++ DuplicateFinder/DuplicateFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateFinder
+{
+    public class DuplicateFinder
+    {
+        /// <summary>
+        /// Retrieves the duplicate in a given array of n numbers that contains all numbers between 1 to n-1
+        /// </summary>
+        /// <param name="array">The input sequence</param>
+        /// <param name="n">The length of the input sequence</param>
+        /// <returns></returns>
+        public int FindTheDuplicate(List<int> array, int n)
+        {
+            try
+            {
+                // Use Gauss sum to get the sum of the (1, n-1) sequence
+                long gaussSum = (long)n * (n - 1) / 2;
+                // Get the sum of the input sequence
+                long currentSum = 0;
+                foreach (int nr in array)
+                    currentSum += nr;
+                // The duplicate element is the difference between the 2 sums
+                return (int)(currentSum - gaussSum);
+            }
+            catch (OutOfMemoryException)
+            {
+                // For test's sake try a smaller n
+                return -1;
+            }
+        }
+    }
+}
---
+++ DuplicateFinderTest/DuplicateFinderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using DuplicateFinder;
+
+namespace DuplicateFinderTest
+{
+    [TestClass]
+    public class DuplicateFinderTest
+    {
+        [TestMethod]
+        public void n100000Duplicate8ShouldReturn8()
+        {
+            DuplicateFinder.DuplicateFinder duplicateTest = new DuplicateFinder.DuplicateFinder();
+            int n = 100000;
+            List<int> array = new List<int>(Enumerable.Range(1, n - 1));
+            // Duplicate 8
+            array.Add(8);
+            var res = duplicateTest.FindTheDuplicate(array, n);
+            Assert.AreEqual(8, res);
+        }
+    }
+}
